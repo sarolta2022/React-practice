eeee45e27e0a789b1cea862e940a859a3b1d2818
sarolta2022 <saroltamail@protonmail.com>
2022-04-20 13:27:03 +0200

    Add Class and AddUser comps with module.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import AddUser from "./components/Users/AddUser";
+import Card from "./components/UI/Card";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
